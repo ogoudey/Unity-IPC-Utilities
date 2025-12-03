f2a81843b59a2c084e7ba5a2d4b96aea49c7086f
Olin <olin.goog@gmail.com>
2025-12-03 17:10:30 -0500

    updates

--- Server/PathFollower.cs
+++ Server/PathFollower.cs
@@ -8,15 +8,21 @@ using System.Collections.Generic;
 
 public class PathFollower : Server
 {   
+    [SerializeField] public float speed = 1f;
+    
     private float[,] cachedHeightmap;
     private TcpListener listener;
     private Thread serverThread;
     private bool running = true;
-
-    public float px, pz;
-    public float px_i, pz_i;
+    private float px, pz;
+    private float px_i, pz_i;
     public float v_i;
-    public float theta;
+    private float theta;
+	
+	
+    
+    
+    
     public bool follow = false;
     void Start()
     {
@@ -27,13 +33,18 @@ public class PathFollower : Server
 
     protected override void HandleClient(TcpClient client)
     {
-        Debug.Log("Request received");
+
         using (NetworkStream stream = client.GetStream())
         {
             byte[] buffer = new byte[4096];
             int count = stream.Read(buffer, 0, buffer.Length);
             string message = Encoding.UTF8.GetString(buffer, 0, count).Trim();
             Debug.Log($"Request received: {message}");
+            if (message == "STOP"){
+            	follow = false;
+            	Send(stream, $"{px} {pz}\n");
+            	return;
+            }
             string[] coords = message.Split(" ");
             foreach (var c in coords) Debug.Log($"coord = '{c}'");
             px_i = float.Parse(coords[0]);
@@ -60,9 +71,9 @@ public class PathFollower : Server
             v_i = (float) Math.Sqrt(Math.Pow(px_i - px, 2) + Math.Pow(pz_i - pz, 2));
             theta = Mathf.Atan2(px_i - px, pz_i - pz) * Mathf.Rad2Deg;
             //theta = (float) Math.Atan2(px_i - px, pz_i - pz) * (float) (180.0 / Math.PI);
-            Debug.Log($"arctan({px_i} - {px}/{pz_i} - {pz}) = {Mathf.Atan2(px_i - px, pz_i - pz)} => {theta}");
+            //Debug.Log($"arctan({px_i} - {px}/{pz_i} - {pz}) = {Mathf.Atan2(px_i - px, pz_i - pz)} => {theta}");
             this.transform.rotation = Quaternion.Euler(0f, theta, 0f);
-            this.transform.position += transform.forward * 1f * Time.deltaTime;
+            this.transform.position += transform.forward * speed * Time.deltaTime;
         }
         
     }
