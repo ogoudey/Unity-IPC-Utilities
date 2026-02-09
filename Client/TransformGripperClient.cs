ee0b2ea46a60593000dc705f24222464c02ae627
Olin <olin.goog@gmail.com>
2026-02-09 18:22:27 -0500

    Add files via upload
    final file for kinova_teleop

--- Client/TransformGripperClient.cs
+++ Client/TransformGripperClient.cs
@@ -2,8 +2,6 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
-using System.Diagnostics;
 
 [Serializable]
 public class TransformGripperData
@@ -12,18 +10,20 @@ public class TransformGripperData
     public float rx, ry, rz, rw;
     public float sx, sy, sz;
 
-    public float gripper;
-    public TransformGripperData(Transform t, float squeeze)
+    public TransformGripperData(Transform t)
     {
-        px = t.localPosition.x;
-        py = t.localPosition.y;
-        pz = t.localPosition.z;
+        px = t.position.x;
+        py = t.position.y;
+        pz = t.position.z;
 
-        rx = t.localEulerAngles.x;
-        ry = t.localEulerAngles.y;
-        rz = t.localEulerAngles.z;
+        rx = t.rotation.x;
+        ry = t.rotation.y;
+        rz = t.rotation.z;
+        rw = t.rotation.w;
 
-        gripper = squeeze;
+        sx = t.localScale.x;
+        sy = t.localScale.y;
+        sz = t.localScale.z;
     }
 }
 
@@ -31,57 +31,26 @@ public class TransformGripperData
 public class TransformGripperClient : MessageClientBehavior
 {      
     private int transformsSent = 1;
-
-    
     protected override void OnConnected()
     {
-        UnityEngine.Debug.Log("[DebugMessageSender] Connected to Python.");
+        Debug.Log("[DebugMessageSender] Connected to Python.");
         SendMessageString("{\"hello\": \"python\"}");
     }
 
     protected override void OnConnectionFailed(Exception e)
     {
         // Wasting the stream but it's whatevs
-        UnityEngine.Debug.LogError($"[DebugMessageSender] Connection failed: {e.Message}");
+        Debug.LogError($"[DebugMessageSender] Connection failed: {e.Message}");
     }
 
     void Update()
     {
-        if (transformsSent % 1 == 0)
+        if (transformsSent % 2 == 1)
         {
-            // Get other inputs to send {"message": "do thing"}
-
-            if (SteamVR_Actions.default_MenuPress.GetStateDown(SteamVR_Input_Sources.RightHand)) { // Select - the three horizontal lines - is pressed
-                string jsonGo = $"{{\"message\": \"go\"}}";
-                UnityEngine.Debug.Log(jsonGo);
-                SendMessageString($"{jsonGo}");
-                return;
-            }
-
-            if (SteamVR_Actions.default_SystemPress.GetStateDown(SteamVR_Input_Sources.RightHand)) { // Exit -  the button below the touchpad - is pressed
-                string jsonStop = $"{{\"message\": \"stop\"}}";
-                UnityEngine.Debug.Log(jsonStop);
-                SendMessageString($"{jsonStop}");
-                return;
-            }
-
-            float squeezeRight = SteamVR_Actions.default_Squeeze.GetAxis(
-                SteamVR_Input_Sources.RightHand
-            );
-            // Other: send the transform...
-
-            
-
-
-
-            TransformGripperData data = new TransformGripperData(this.transform, squeezeRight);
+            TransformGripperData data = new TransformGripperData(this.transform);
             string json = JsonUtility.ToJson(data);
-            if (transformsSent % 10 == 0)
-            {
-                UnityEngine.Debug.Log(json);
-            }
+            Debug.Log(json);
             SendMessageString($"{json}");   
         }
-        transformsSent ++;
     }
 }
